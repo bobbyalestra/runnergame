f4984ffc52a7c1949550a4f15cebb0ed95b2f678
Bobby Alestra <bobbyalestra135@gmail.com>
2021-10-19 12:34:25 -0400

    making a while loop and adding an exit strategy

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
+from sys import exit
 
 pygame.init()
-
 screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Runner')
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.QUIT()
+            exit()
     # Draw all elements and update everything
+
     pygame.display.update()
