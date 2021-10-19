1b3c683ca1da377dfc70ead2c67dd81811fa3fe3
Bobby Alestra <bobbyalestra135@gmail.com>
2021-10-19 12:48:55 -0400

    making a while loop, adding an exit strategy, telling sys to run no higher than 60 FPS, add a base and second surface, added surface sky, and surface ground

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
-test_surface = pygame.image.load('graphics/Sky.png')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.QUIT()
             exit()
-
-    screen.blit(test_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(sky_surface, (0, 0))
 
     pygame.display.update()
     clock.tick(60)
